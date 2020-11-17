315b3ed02c6dc983b69547d6bf192c2cfcaaea4c
David González <lpamad98@gmail.com>
2020-11-17 13:15:55 +0000

    Intento del apartado 4 del ejercicio 1

--- prueba_git.py
+++ prueba_git.py
@@ -5,10 +5,22 @@ Created on Sat Nov 14 18:00:29 2020
 @author: lpama
 """
 
-p= 'Hola Mundo'
+from astropy import *
+from pandas import *
+import seaborn as sns
 
-print (p)
+from astropy.table import Table
+dat = Table.read('Sharks_sgp_e_2_cat_small.fits', format='fits')
+df = dat.to_pandas()
+print(type(df))
 
-D='David es el mejor de mundo'
+"""
+from astropy.io import fits
+from glob import glob
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-print (D)
+archivo_fits = fits.open('Sharks_sgp_e_2_cat_small.fits') #open file
+"""
+sns.jointplot(data=dat, x="ALPHA_J2000", y= "DELTA_J2000", kind=hex)
