019a9d1125b6f53a1a9af752420c54d69ddf6abe
Aurelio Carnero <aurelio.crosell@gmail.com>
2020-11-18 11:58:34 +0000

    Resuelto problema de jointplot y algunos comentarios

--- Prueba_github.py
+++ Prueba_github.py
@@ -14,6 +14,8 @@ dat = Table.read('Sharks_sgp_e_2_cat_small.fits', format='fits')
 df = dat.to_pandas()
 #PRINT(type(df))
 
+#Comment by Aurelio: necesitaras este import de pylab para poder ver la figura
+import pylab as pl
 """
 from astropy.io import fits
 from glob import glob
@@ -23,4 +25,16 @@ import matplotlib.pyplot as plt
 
 archivo_fits = fits.open('Sharks_sgp_e_2_cat_small.fits') #open file
 """
-sns.jointplot(data=pandas.DataFrame, x="ALPHA_J2000", y= "DELTA_J2000", kind=hex)
+#Comment by Aurelio: el problema era que en data, tenias que poner tu DataFrame de entrada, en este caso: df, y de ahi se leen las columnas x,y. 
+#Igualmente, kind tiene que venir entre aspas o comillas, pues es un string.
+#Lo correcto sera:
+plot = sns.jointplot(data=df, x="ALPHA_J2000", y= "DELTA_J2000", kind='hex')
+
+#Excercise: busca la documentacion de jointplot para ver como cambiar el nombre de los ejes, y en vez de ser ALPHA_J2000 y DELTA_J2000, que sea RA y DEC. Con tamano de fuente = 16. 
+#Una pista, la funcion se llama set_axis_labels
+
+#Comment: estas siguiente linea hace que los margenes de los plots se ajusten a la figura
+pl.tight_layout()
+
+#Comment: este comando te muestra la figura, si en vez de eso, quieres guardarla, en vez de show(), debes usar la funcion savefig(). Busca informacion para ver como indicar el nombre del archivo de salida. Cuando tengas hecha la figura, formato jpg o png, me la puedes enviar por email y continuar con los siguientes ejercicios que te habia indicado.
+pl.show()
