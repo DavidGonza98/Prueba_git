3ef43c2e6a44e83ea2dd3d9461000c10ba79b02c
Aurelio Carnero <aurelio.crosell@gmail.com>
2021-02-22 15:12:29 +0000

    Mejoras en el plot

--- astrometria.py
+++ astrometria.py
@@ -58,8 +58,7 @@ for i in range(len(matches)):
 #print(np.max(cosgamma))
 
 #result= sns.jointplot(x=kmag2matched, y=mag1matched, kind="reg", truncate=False, xlim=(5, 25), ylim=(5, 25), color="m", height=7)
-#pl.plot(kmag2matched, mag1matched, ".")
-pl.show()
+pl.plot(kmag2matched, mag1matched, ".")
 #print (result)
 
 mask = kmag2matched>12.3
@@ -75,31 +74,17 @@ mag1Finalajuste= p[0]*kmagFinal + p[1]
 
 
 
-x = mag1Finalajuste - kmagFinal
+#x = mag1Finalajuste - kmagFinal
 
-mag1Finalajuste1= kmagFinal + x[0]
+#mag1Finalajuste1= kmagFinal + x[0]
 
-pl.plot(kmagFinal, mag1Finalajuste, kmagFinal, mag1Finalajuste1)
+pl.plot(kmagFinal, mag1Finalajuste) #, kmagFinal, mag1Finalajuste1)
+pl.plot([10,20],[10,20])
 
+pl.plot(kmag2matched, mag1matched-p[1], ".")
+pl.show()
 #p2= pl.polyfit(kmagFinal, mag1Finalajuste1, 1)
 
 #print (p2[0], p2[1])
-
-datos_calibrados = open('Sharks_sgp_e_2_cat_small.fits', 'w')
-
-#print (mag1Finalajuste1)
-
-#df['MAG_AUTO']= mag1Finalajuste1
-
-
-for i in range(len(mag1Finalajuste1)):
-    
-    mag1[i]= str(mag1[i]).replace(str(mag1[i]), str(mag1Finalajuste1[i]))
-    
-    
-  
-
-
-datos_calibrados.close()
 
 
